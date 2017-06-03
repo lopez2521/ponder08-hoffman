d9df75056245d32569cd3e4558ef36c6559b0f13
Kimberly Stowe <stowekm@byui.edu>
2017-06-03 08:37:18 -0700

    added comments to bnode

--- hoffman/hoffman/bnode.h
+++ hoffman/hoffman/bnode.h
@@ -1,1 +1,14 @@
-#pragma once
+/***********************************************************************
+* Module:
+*    Week 08, Binary Node
+*    Brother JonesL, CS 235
+* Author:
+*    Shayla Nelson, Kimberly Stowe, Bryan Lopez & Matt Burr
+* Summary:
+*    This program 
+************************************************************************/
+
+#ifndef BINARYNODE_H
+#define BINARYNODE_H
+
+#endif // BINARYNODE_H
